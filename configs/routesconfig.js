44d2f5d05e7f09c936846480e50568d024383f5c
Maximiliano Carrizo <maximiliano.carrizo@whiteprompt.com>
2019-02-13 16:22:04 -0300

    Routes

--- configs/routesconfig.js
+++ configs/routesconfig.js
@@ -1,16 +1,22 @@
-const logger = require('configs/loggerconfig')(module);
 const { categories,
         sidebar,
         home,
         footer,
 				auth, 
-				token,
         productspictures} = require('app/controllers');
 const passport = require('passport');
 
 const localAuth = passport.authenticate('login-local',{ session:false });
-const facebookAuth = passport.authenticate('login-facebook',{ session:false });
-const googleAuth = passport.authenticate('login-google',{ session:false, scope: ['profile','email'] });
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+const facebookAuth = (req, res, next) => {
+	const authenticator = passport.authenticate('login-facebook', { session: false, state: req.query.socketId });
+	authenticator(req, res, next);
+};
+const googleAuth = (req, res, next) => {
+	const authenticator = passport.authenticate('login-google', { session: false, scope: ['profile', 'email'], state: req.query.socketId });
+	authenticator(req, res, next);
+};
 
 const testWorkflow = (fn,req,res,next) => {
     fn(req,res,next)
@@ -20,7 +26,6 @@ const testWorkflow = (fn,req,res,next) => {
 };
 
 const noTestWorkflow = (fn,req,res,next) => {
-
     req.db.transaction()
     .then(t => {
         req.trx = t;
@@ -48,8 +53,15 @@ const wrap = (fn) => {
 
 module.exports = (app) => {
 
+	app.post('/auth/local', localAuth, wrap(auth.login));
+	app.get(`/auth/google`, googleAuth);
+	app.get(`/auth/facebook`,facebookAuth);
+
+	app.get('/auth/facebook/callback', facebookAuth, wrap(auth.social));
+	app.get('/auth/google/callback', googleAuth, wrap(auth.social));
 
 	app.get('/categories', wrap(categories.list));
+	app.post('/admin/categories', jwtAuth, wrap(categories.create));
 
 	app.get('/productspictures/:picture_id', wrap(productspictures.get));
 
@@ -61,20 +73,6 @@ module.exports = (app) => {
 	app.get('/search/:search', wrap(home.get_search));
 	app.get('/:category', wrap(home.get_category));
 
-	app.post('/auth/local', localAuth, wrap(auth.login));
-	app.get(`/auth/google`, (req, res, next) => {
-		const authenticator = passport.authenticate('login-google', { session: false, scope: ['profile', 'email'], state: req.query.socketId });
-		authenticator(req, res, next);
-	});
-	app.get(`/auth/facebook`, (req, res, next) => {
-		const authenticator = passport.authenticate('login-facebook', { session: false, state: req.query.socketId });
-		authenticator(req, res, next);
-	});
-
-	app.get('/auth/facebook/callback', facebookAuth, wrap(auth.social));
-	app.get('/auth/google/callback', googleAuth, wrap(auth.social));
-
-	app.post('/admin/categories', passport.authenticate('jwt', { session: false }), wrap(categories.create));
 
 
 };
